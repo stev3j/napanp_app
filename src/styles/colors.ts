bcaca16617d6cd84a2af4951242c63be4ac74723
stev3j <mygajeong@gmail.com>
2024-03-12 10:32:16 +0900

    design: colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,9 @@
+const colors = {
+    white: '#FFFFFF',
+    text_gray_100: '#D4D4D4',
+    hint_gray_200: '#9B9B9B',
+    container_gray_700: '#2C2C2C',
+    background_gray_900: '#191919',
+}
+
+export default colors;
