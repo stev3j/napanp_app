26570c2d5ec04178ebb68e1510a75d242793e7e8
stev3j <mygajeong@gmail.com>
2024-03-12 16:00:50 +0900

    design: add black & primary

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,9 +1,13 @@
 const colors = {
+    black: '#000000',
     white: '#FFFFFF',
+
     text_gray_100: '#D4D4D4',
     hint_gray_200: '#9B9B9B',
     container_gray_700: '#2C2C2C',
     background_gray_900: '#191919',
+
+    primary: '#F1D160'
 }
 
 export default colors;
