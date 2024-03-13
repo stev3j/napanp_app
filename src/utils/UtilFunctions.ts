8b23cff199df8fbf4850334d84db8f2585aad1df
stev3j <mygajeong@gmail.com>
2024-03-13 13:51:19 +0900

    feat: padding horizontal 40 util function

--- src/utils/UtilFunctions.ts
+++ src/utils/UtilFunctions.ts
@@ -3,3 +3,8 @@ import styled from "styled-components/native";
 export const Spacer = styled.View`
     flex: 1;
 `
+
+export const PaddingHorizontal40 = styled.View`
+    padding-left: 40px;
+    padding-right: 40px;
+`
