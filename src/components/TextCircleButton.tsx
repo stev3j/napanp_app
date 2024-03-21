5a4ca205ed3d6133faa70baec96ba044321dacde
조승완 <103572569+stev3j@users.noreply.github.com>
2024-03-21 11:39:29 +0900

    feat: TextCircleButton Component

---
+++ src/components/TextCircleButton.tsx
@@ -0,0 +1,35 @@
+import styled from "styled-components/native";
+import colors from "../styles/colors";
+import { Clickable } from "../utils/UtilFunctions";
+
+type TextCircleButtonType = {
+    label: string
+    onPress: () => void
+}
+
+const TextCircleButton = ({label, onPress}: TextCircleButtonType) => {
+    return (
+        <Clickable onPress={onPress}>
+            <Frame>
+                <Label>{label}</Label>
+            </Frame>
+        </Clickable>
+        
+    );
+}
+
+const Frame = styled.View`
+    border-radius: 100px;
+    width: 60px;
+    height: 60px;
+    background-color: ${colors.container_gray_700};
+    align-items: center;
+    justify-content: center;
+`
+
+const Label = styled.Text`
+    font-size: 14px;
+    color: ${colors.white};
+`
+
+export default TextCircleButton;
