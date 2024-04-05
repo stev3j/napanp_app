f516a0b1aaf98a15366de828d20a902ba6800080
조승완 <103572569+stev3j@users.noreply.github.com>
2024-04-05 10:38:29 +0900

    feat: alert component

--- src/utils/UtilFunctions.ts
+++ src/utils/UtilFunctions.ts
@@ -1,5 +1,9 @@
 import styled from "styled-components/native";
 import colors from "../styles/colors";
+import Sound from "react-native-sound";
+import AudioPaths from "../assets/audios/AudioPaths";
+import { Alert } from "react-native";
+import { resetTimer } from "../redux/slices/timer";
 
 export const Spacer = styled.View`
     flex: 1;
@@ -34,3 +38,21 @@ export const CircleButton = styled.Image`
     width: 60px;
     height: 60px;
 `
+
+export const MyAlert = (dispatch: any, setIsSetTimer: any, setPlay: any) => {
+    Alert.alert(
+        "정말 타이머를 초기화 하시겠습니까?",
+        "", [{
+            text: "아니요",
+            style: "cancel"
+        }, {
+            text: "네",
+            onPress: () => {
+                setPlay(false)
+                dispatch(resetTimer())
+                setIsSetTimer(false)
+            },
+        },
+        ], { cancelable: false }
+    );
+}
