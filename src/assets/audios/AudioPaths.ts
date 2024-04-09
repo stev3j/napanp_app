66cda7ef0caefe599d4881a57984a3e5299d7893
조승완 <103572569+stev3j@users.noreply.github.com>
2024-04-09 10:13:52 +0900

    fix: mp3 to wav

---
+++ src/assets/audios/AudioPaths.ts
@@ -0,0 +1,11 @@
+
+const AudioPaths = {
+  Rain: '/Users/stev3j/Desktop/Programing/RN/projects/napanp_app/src/assets/audios/rain.mp3',
+  Alarm: '/Users/stev3j/Desktop/Programing/RN/projects/napanp_app/src/assets/audios/alarm.mp3',
+  RainWav: '/Users/stev3j/Desktop/Programing/RN/projects/napanp_app/src/assets/audios/rain.wav',
+  AlarmWav: '/Users/stev3j/Desktop/Programing/RN/projects/napanp_app/src/assets/audios/alarm.wav',
+  // PROBLEM : 이 사운드를 내 로컬파일이 아닌, 웹 상에 올려야 함
+  //   Rain: 'http://www.soundjay.com/nature/rain-07.mp3',
+};
+
+export default AudioPaths;
