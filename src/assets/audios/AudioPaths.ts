62dc3be09b874d98150b33ec21b37018ca4bdf34
조승완 <103572569+stev3j@users.noreply.github.com>
2024-06-25 12:18:51 +0900

    commit: 중간 커밋

--- src/assets/audios/AudioPaths.ts
+++ src/assets/audios/AudioPaths.ts
@@ -1,7 +1,5 @@
 
 const AudioPaths = {
-  Rain: '/Users/stev3j/Desktop/Programing/RN/projects/napanp_app/src/assets/audios/rain.mp3',
-  Alarm: '/Users/stev3j/Desktop/Programing/RN/projects/napanp_app/src/assets/audios/alarm.mp3',
   RainWav: '/Users/stev3j/Desktop/Programing/RN/projects/napanp_app/src/assets/audios/rain.wav',
   AlarmWav: '/Users/stev3j/Desktop/Programing/RN/projects/napanp_app/src/assets/audios/alarm.wav',
   // PROBLEM : 이 사운드를 내 로컬파일이 아닌, 웹 상에 올려야 함
