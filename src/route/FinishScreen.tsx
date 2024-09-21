79edc98d9bf9268b6c2607678ec596acb36faa62
조승완 <103572569+stev3j@users.noreply.github.com>
2024-09-21 12:06:26 +0900

    fix: sound problem

--- src/route/FinishScreen.tsx
+++ src/route/FinishScreen.tsx
@@ -1,158 +1,192 @@
-import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
-import { Background, ButtonContainer, Spacer } from "../utils/UtilFunctions";
-import RootStackParamList from "../navigation/RootStackParamList";
-import { StackNavigationProp } from '@react-navigation/stack'
-import styled from "styled-components/native";
-import colors from "../styles/colors";
-import ImagePaths from "../assets/images/ImagePaths";
-import TerminateButton from "../assets/buttons/TerminateButton";
-import ReloadButton from "../assets/buttons/ReloadButton";
-import { Button, LogBox } from "react-native";
-import Sound from "react-native-sound";
-import { useEffect, useRef } from "react";
-import { useAppDispatch } from "../redux/hook";
-import { setTimer } from "../redux/slices/timer";
-// import AudioPaths from "../assets/audios/AudioPaths";
+import { RouteProp, useNavigation, useRoute } from "@react-navigation/native"
+import { Background, ButtonContainer, Spacer } from "../utils/UtilFunctions"
+import RootStackParamList from "../navigation/RootStackParamList"
+import { StackNavigationProp } from "@react-navigation/stack"
+import styled from "styled-components/native"
+import colors from "../styles/colors"
+import ImagePaths from "../assets/images/ImagePaths"
+import TerminateButton from "../assets/buttons/TerminateButton"
+import ReloadButton from "../assets/buttons/ReloadButton"
+import { Button, LogBox } from "react-native"
+import Sound from "react-native-sound"
+import { useEffect, useRef, useState } from "react"
+import { useAppDispatch } from "../redux/hook"
+import { setTimer } from "../redux/slices/timer"
+import AudioPaths from "../assets/audios/AudioPaths"
 
 const FinishScreen = ({ route }: any) => {
-    LogBox.ignoreLogs([
-        'Non-serializable values were found in the navigation state.',
-    ]);
+	LogBox.ignoreLogs([
+		"Non-serializable values were found in the navigation state.",
+	])
 
-    const { setPlay, setIsSetTimer } = route.params;
+	const { setPlay, setIsSetTimer } = route.params
 
-    const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
+	const navigation = useNavigation<StackNavigationProp<RootStackParamList>>()
 
-    // Sound
-    const alarmSound = useRef<Sound>(
-        new Sound('alarm.wav', Sound.MAIN_BUNDLE, error => {
-            if (error) {
-                console.error("error", error);
-            }
-        })
-    );
+	/** ---------------------------- SOUND ---------------------------- */
 
-    useEffect(() => {
-        setTimeout(() => {
-            console.log("알람이 울리고 있어요!");
-            // console.log(`Props: ${onPlay} ${isSetTimer}`);
-            alarmSound.current.setNumberOfLoops(-1).play()
-        }, 1500)
-    })
+	const [soundReady, setSoundReady] = useState(false)
+	const alarmSound = useRef<Sound | null>(null)
 
-    const dispatch = useAppDispatch()
+	useEffect(() => {
+		console.log("Alarm Sound 초기화")
 
-    // View
-    return (
-        <Background>
-            <AlarmTitle/>
-            <Title>일어나요!</Title>
-            <CatImage source={ImagePaths.WakeupCat}/>
-            <Buttons onPressStop={() => {
-                alarmSound.current.stop()
-                navigation.goBack()
-            }} onPress5Minute={() => {
-                alarmSound.current.stop()
-                dispatch(setTimer({minute: '05', second: '00'}))
-                setIsSetTimer(true)
-                setPlay(true)
-                navigation.goBack()
-            }}/>
-        </Background>
-    );
+		const sound = new Sound(
+			AudioPaths.alarm,
+			Sound.MAIN_BUNDLE,
+			(error) => {
+				if (error) {
+					console.error("error", error)
+					return
+				}
+				// 사운드 파일이 로드된 후
+				setSoundReady(true) // 사운드가 준비된 상태임을 알림
+			}
+		)
+
+		alarmSound.current = sound
+
+		return () => {
+			if (alarmSound.current) {
+				alarmSound.current.release() //
+			}
+		}
+	}, [])
+
+	// 플레이 버튼 눌렀을 때 실행
+	const handlePlaySound = () => {
+		if (soundReady && alarmSound.current) {
+			alarmSound.current.setNumberOfLoops(-1).play()
+		}
+	}
+
+	// 스탑 버튼 눌렀을 때 실행
+	const handleStopSound = () => {
+		if (alarmSound.current) {
+			alarmSound.current.stop(() => {
+				console.log("Sound stopped.")
+			})
+		}
+	}
+
+	/** ---------------------------- SOUND ---------------------------- */
+
+	useEffect(() => {
+		console.log("알람이 울리고 있어요!")
+		handlePlaySound()
+	})
+
+	const dispatch = useAppDispatch()
+
+	// View
+	return (
+		<Background>
+			<AlarmTitle />
+			<Title>일어나요!</Title>
+			<CatImage source={ImagePaths.WakeupCat} />
+			<Buttons
+				onPressStop={() => {
+					handleStopSound()
+					navigation.goBack()
+				}}
+				onPress5Minute={() => {
+					handleStopSound()
+					dispatch(setTimer({ minute: "05", second: "00" }))
+					setIsSetTimer(true)
+					setPlay(true)
+					navigation.goBack()
+				}}
+			/>
+		</Background>
+	)
 }
 
-export default FinishScreen;
+export default FinishScreen
 
 interface ButtonsType {
-    onPressStop: () => void,
-    onPress5Minute: () => void
+	onPressStop: () => void
+	onPress5Minute: () => void
 }
 
-const Buttons = ({onPressStop, onPress5Minute}: ButtonsType) => {
-    return (
-        <ButtonContainer>
-            <ButtonLabelContainer>
-                <TerminateButton onPress={onPressStop}/>
-                   
-                <ButtonLabel>종료하기</ButtonLabel>
-            </ButtonLabelContainer>
-            
-            <Spacer/>
+const Buttons = ({ onPressStop, onPress5Minute }: ButtonsType) => {
+	return (
+		<ButtonContainer>
+			<ButtonLabelContainer>
+				<TerminateButton onPress={onPressStop} />
 
-            <ButtonLabelContainer>
-                <ReloadButton onPress={onPress5Minute}/>
-                <ButtonLabel>5분 더</ButtonLabel>
-            </ButtonLabelContainer>
-            
-        </ButtonContainer>
-    )
+				<ButtonLabel>종료하기</ButtonLabel>
+			</ButtonLabelContainer>
+
+			<Spacer />
+
+			<ButtonLabelContainer>
+				<ReloadButton onPress={onPress5Minute} />
+				<ButtonLabel>5분 더</ButtonLabel>
+			</ButtonLabelContainer>
+		</ButtonContainer>
+	)
 }
 
 const AlarmTitle = () => {
-    const now = new Date();
-    const formattedTime = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
-    
-    return (
-        <AlramContainer>
-            <AlramIcon source={require('../assets/images/ic_alram.png')}/>
-            <SubTitle>{foramtTime(formattedTime)}</SubTitle> 
-        </AlramContainer>
-    );
+	const now = new Date()
+	const formattedTime = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`
+
+	return (
+		<AlramContainer>
+			<AlramIcon source={require("../assets/images/ic_alram.png")} />
+			<SubTitle>{foramtTime(formattedTime)}</SubTitle>
+		</AlramContainer>
+	)
 }
 
 const foramtTime = (time: string): string => {
-    const array = time.split(':')
-    let hour = +array[0]
-    let minute = +array[1]
+	const array = time.split(":")
+	let hour = +array[0]
+	let minute = +array[1]
 
-    if (hour < 12) {
-        const result = `오전 ${hour}시 ${minute}분`
-        return result
-    } else {
-        const result = `오후 ${hour-12}시 ${minute}분`
-        return result
-    }
+	if (hour < 12) {
+		const result = `오전 ${hour}시 ${minute}분`
+		return result
+	} else {
+		const result = `오후 ${hour - 12}시 ${minute}분`
+		return result
+	}
 }
 
 const AlramContainer = styled.View`
-    flex-direction: row;
-    align-items: center;
-    margin-top: 70px;
+	flex-direction: row;
+	align-items: center;
+	margin-top: 70px;
 `
 
 const AlramIcon = styled.Image`
-   width: 12px;
-   height: 14px;
-   margin-right: 8px;
+	width: 12px;
+	height: 14px;
+	margin-right: 8px;
 `
 
 const SubTitle = styled.Text`
-    font-size: 20px;
-    color: ${colors.text_gray_100};
+	font-size: 20px;
+	color: ${colors.text_gray_100};
 `
 
-
 const Title = styled.Text`
-    font-size: 80px;
-    color: ${colors.text_gray_100};
+	font-size: 80px;
+	color: ${colors.text_gray_100};
 `
 
 const CatImage = styled.Image`
-    margin-top: 20px;
-    width: 240px;
-    height: 220px;
+	margin-top: 20px;
+	width: 240px;
+	height: 220px;
 `
 
-
-
 const ButtonLabelContainer = styled.View`
-    align-items: center;
-    gap: 10px;
+	align-items: center;
+	gap: 10px;
 `
 
 const ButtonLabel = styled.Text`
-    font-size: 16px;
-    color: ${colors.text_gray_100};
+	font-size: 16px;
+	color: ${colors.text_gray_100};
 `
-
--- src/route/HomeScreen.tsx
+++ src/route/HomeScreen.tsx
@@ -1,272 +1,311 @@
-import React, { useState, useEffect, useRef } from 'react';
-import styled from 'styled-components/native';
-import colors from '../styles/colors';
-import NapnapModal from '../components/NapanpModal';
-import { Background, ButtonContainer, Clickable, MyAlert, Spacer } from '../utils/UtilFunctions';
-import MusicContainer from '../components/MusicContainer';
-import { useAppDispatch, useAppSelector } from '../redux/hook';
-import { minusMinute, minusSecond, setSecond, setTimer } from '../redux/slices/timer';
-import BackgroundTimer from 'react-native-background-timer';
-import TextCircleButton from '../assets/buttons/TextCircleButton';
-import Sound from 'react-native-sound';
-import { useNavigation } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
-import RootStackParamList from '../navigation/RootStackParamList';
-import ImagePaths from '../assets/images/ImagePaths';
-import PlayButton from '../assets/buttons/PlayButton';
-import LottieView from 'lottie-react-native';
-import AnimationPaths from '../assets/animatinos/AnimationPaths';
-import { LogBox, Platform } from 'react-native';
-import AudioPaths from '../assets/audios/AudioPaths';
+import React, { useState, useEffect, useRef } from "react"
+import styled from "styled-components/native"
+import colors from "../styles/colors"
+import NapnapModal from "../components/NapanpModal"
+import {
+	Background,
+	ButtonContainer,
+	Clickable,
+	MyAlert,
+	Spacer,
+	foramtTime,
+} from "../utils/UtilFunctions"
+import MusicContainer from "../components/MusicContainer"
+import { useAppDispatch, useAppSelector } from "../redux/hook"
+import {
+	minusMinute,
+	minusSecond,
+	setSecond,
+	setTimer,
+} from "../redux/slices/timer"
+import BackgroundTimer from "react-native-background-timer"
+import TextCircleButton from "../assets/buttons/TextCircleButton"
+import Sound from "react-native-sound"
+import { useNavigation } from "@react-navigation/native"
+import { StackNavigationProp } from "@react-navigation/stack"
+import RootStackParamList from "../navigation/RootStackParamList"
+import ImagePaths from "../assets/images/ImagePaths"
+import PlayButton from "../assets/buttons/PlayButton"
+import LottieView from "lottie-react-native"
+import AnimationPaths from "../assets/animatinos/AnimationPaths"
+import { LogBox, Platform, View } from "react-native"
+import AudioPaths from "../assets/audios/AudioPaths"
 
 const HomeScreen = () => {
-    LogBox.ignoreLogs([
-        '`new NativeEventEmitter()` was called with a non-null argument without the required `addListener` method.',
-        '`new NativeEventEmitter()` was called with a non-null argument without the required `removeListeners` method.',
-    ]);
+	LogBox.ignoreLogs([
+		"`new NativeEventEmitter()` was called with a non-null argument without the required `addListener` method.",
+		"`new NativeEventEmitter()` was called with a non-null argument without the required `removeListeners` method.",
+	])
 
-    // navigation
-    const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
+	// states
+	const [isModalVisible, setModalVisible] = useState(false)
+	const [onPlay, setPlay] = useState(false)
+	const [isSetTimer, setIsSetTimer] = useState(false)
+	const [endTime, setEndTime] = useState("시간을 정해주세요")
 
-    // timer
-    const timer = useAppSelector(state => state.timer.timer);
-    const dispatch = useAppDispatch()
-    
-    // states
-    const [ isModalVisible, setModalVisible ] = useState(false)
-    const [ onPlay, setPlay ] = useState(false)
-    const [ isSetTimer, setIsSetTimer ] = useState(false) 
-    const [ endTime, setEndTime ] = useState('시간을 정해주세요')
+	// navigation
+	const navigation = useNavigation<StackNavigationProp<RootStackParamList>>()
 
-    // sound
-    const rainSound = useRef<Sound>(
-        // ## iOS
-        new Sound('rain.wav', Sound.MAIN_BUNDLE, error => {
-            if (error) {
-                console.error("error", error);
-            }
-        })
-        
-        // ## Android
-        // new Sound(AudioPaths.rain, '', error => {
-        //     if (error) {
-        //         console.error("error", error);
-        //     }
-        // })
-    );
+	/** ---------------------------- SOUND ---------------------------- */
 
-    // endTime 설정
-    useEffect(() => {
-        if (isSetTimer == true) {
-            const now = new Date();
-            const formattedTime = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
-            setEndTime(foramtTime(formattedTime, timer))
-        } else {
-            setEndTime('시간을 정해주세요')
-        }
-    }, [isSetTimer])
+	const [soundReady, setSoundReady] = useState(false)
+	const rainSound = useRef<Sound | null>(null)
 
-    // timer count (second가 줄어들 때마다)
-    useEffect(() => {
-        console.log(`minute: ${+timer.minute}, second: ${+timer.second}`);
-        const finishCallback = () => {
-            console.log('종료!');
-            BackgroundTimer.stopBackgroundTimer()
-            rainSound.current.stop();
-            navigation.navigate('Finish', {setPlay: setPlay, setIsSetTimer: setIsSetTimer})
-            setPlay(false)
-            setIsSetTimer(false)
-        }
-        handlingTimerStop(timer, dispatch, onPlay, finishCallback)
-    }, [+timer.second])
+    // Rain Sound 초기화
+	useEffect(() => {
+        console.log("Rain Sound 초기화")
 
-    // while playing
-    useEffect(() => {
-        handlingTimerStart(onPlay, dispatch, rainSound)
-    }, [onPlay])
+		const sound = new Sound(AudioPaths.rain, Sound.MAIN_BUNDLE, (error) => {
+			if (error) {
+				console.error("error", error)
+				return
+			}
+			// 사운드 파일이 로드된 후
+			setSoundReady(true) // 사운드가 준비된 상태임을 알림
+		})
 
-    // Views
-    return (
-        <Background>
-            <AlarmTitle endTime={endTime}/>
-            <Time isSetTimer={isSetTimer} dispatch={dispatch} setModalVisible={setModalVisible} timer={timer}/>
-            
-            {/* <CatImage source={ImagePaths.BasicCat}/> */}
-            <LottieView style={{marginTop: 20, width: 230, height: 230}} source={AnimationPaths.SleepyCat} autoPlay loop />
-            
-            <Buttons isSetTimer={isSetTimer} dispatch={dispatch} setIsSetTimer={setIsSetTimer} onPlay={onPlay} setPlay={setPlay} />
+		rainSound.current = sound
 
-            <MusicContainer
-                style={{marginBottom: 8}}
-                iconPath={ImagePaths.NotificationIcon}
-                title='배경음악' 
-                selectedItem='추적추적 빗소리'/>
-            <MusicContainer
-                style={{marginBottom: 60}}
-                iconPath={ImagePaths.MusicIcon}
-                title='타이머 종료음' 
-                selectedItem='알람음 1'/>
+		return () => {
+			if (rainSound.current) {
+				rainSound.current.release() // 
+			}
+		}
+	}, [])
 
-            <NapnapModal 
-                isModalVisible={isModalVisible}
-                setModalVisible={setModalVisible}
-                setIsSetTimer={setIsSetTimer}
-            />
-        </Background>
-    )
+	// 플레이 버튼 눌렀을 때 실행
+	const handlePlaySound = () => {
+		if (soundReady && rainSound.current) {
+			rainSound.current.setNumberOfLoops(-1).play()
+		}
+	}
+
+	// 스탑 버튼 눌렀을 때 실행
+	const handleStopSound = () => {
+		if (rainSound.current) {
+			rainSound.current.stop(() => {
+				console.log("Sound stopped.")
+			})
+		}
+	}
+
+	/** ---------------------------- SOUND ---------------------------- */
+
+	/** ---------------------------- TIMER ---------------------------- */
+
+	const timer = useAppSelector((state) => state.timer.timer)
+	const dispatch = useAppDispatch()
+
+	const handlingTimerPlay = (onPlay: any, dispatch: any, soundRef: any) => {
+		const startTimer = () => {
+			BackgroundTimer.runBackgroundTimer(() => {
+				dispatch(minusSecond())
+			}, 1000)
+		}
+
+		if (onPlay) {
+			console.log("background sound on!")
+			startTimer()
+			handlePlaySound()
+		} else {
+			console.log("background sound off..")
+			BackgroundTimer.stopBackgroundTimer()
+			handleStopSound()
+		}
+	}
+
+	// Second가 -1이 될 때마다 실행
+	useEffect(() => {
+		console.log(`minute: ${+timer.minute}, second: ${+timer.second}`)
+
+		const finishCallback = () => {
+			console.log("종료!")
+			BackgroundTimer.stopBackgroundTimer()
+			handleStopSound()
+			navigation.navigate("Finish", {
+				setPlay: setPlay,
+				setIsSetTimer: setIsSetTimer,
+			})
+			setPlay(false)
+			setIsSetTimer(false)
+		}
+		handlingTimerStop(timer, dispatch, onPlay, finishCallback)
+	}, [+timer.second])
+
+	// while playing
+	useEffect(() => {
+		handlingTimerPlay(onPlay, dispatch, rainSound)
+	}, [onPlay])
+
+	/** ---------------------------- TIMER ---------------------------- */
+
+	// endTime 설정
+	useEffect(() => {
+		if (isSetTimer == true) {
+			const now = new Date()
+			const formattedTime = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`
+			setEndTime(foramtTime(formattedTime, timer))
+		} else {
+			setEndTime("시간을 정해주세요")
+		}
+	}, [isSetTimer])
+
+	// Views
+	return (
+		<Background>
+			<AlarmTitle endTime={endTime} />
+			<Time
+				isSetTimer={isSetTimer}
+				dispatch={dispatch}
+				setModalVisible={setModalVisible}
+				timer={timer}
+			/>
+
+			<LottieView
+				style={{ marginTop: 20, width: 230, height: 230 }}
+				source={AnimationPaths.SleepyCat}
+				autoPlay
+				loop
+			/>
+
+			<Buttons
+				isSetTimer={isSetTimer}
+				onPlay={onPlay}
+				leftOnPress={() => {
+					MyAlert(dispatch, setIsSetTimer, setPlay)
+				}}
+				rightOnPress={() => {
+					setPlay(!onPlay)
+				}}
+			/>
+
+			<MusicContainer
+				style={{ marginBottom: 8 }}
+				iconPath={ImagePaths.NotificationIcon}
+				title="배경음악"
+				selectedItem="추적추적 빗소리"
+			/>
+			<MusicContainer
+				style={{ marginBottom: 60 }}
+				iconPath={ImagePaths.MusicIcon}
+				title="타이머 종료음"
+				selectedItem="알람음 1"
+			/>
+
+			<NapnapModal
+				isModalVisible={isModalVisible}
+				setModalVisible={setModalVisible}
+				setIsSetTimer={setIsSetTimer}
+			/>
+		</Background>
+	)
 }
 
-export default HomeScreen;
+export default HomeScreen
 
-interface Timer {
-    minute: string;
-    second: string;
+const handlingTimerStop = (
+	timer: any,
+	dispatch: any,
+	onPlay: boolean,
+	finishCallBack: () => void
+) => {
+	if (+timer.second == 0 || Number.isNaN(+timer.second - 1)) {
+		if (+timer.minute > 0) {
+			if (+timer.second == 0) {
+				setTimeout(() => {
+					dispatch(minusMinute())
+					dispatch(setSecond({ second: "60" }))
+				}, 1000)
+			} else {
+				dispatch(minusMinute())
+				dispatch(setSecond({ second: "59" }))
+			}
+		} else if (onPlay) {
+			finishCallBack()
+		}
+	}
 }
 
-// 받아온 시간 포맷하기 (ex. 11:11:30)
-const foramtTime = (time: string, timer: Timer): string => {
-    const array = time.split(':')
-    let hour = +array[0]
-    let minute = +array[1]
-    let second = +array[2]
-
-    second = second + (+timer.second)
-
-    if (second > 59) { minute = minute + 1 }
-
-    minute = minute + (+timer.minute)
-
-    if (minute > 59) {
-        hour = hour + 1
-        minute = minute - 60
-    }
-
-    if (hour < 12) {
-        const result = `오전 ${hour}시 ${minute}분`
-        return result
-    } else {
-        const result = `오후 ${hour-12}시 ${minute}분`
-        return result
-    }
-}
-
-const handlingTimerStop = (timer: any, dispatch: any, onPlay: boolean, finishCallBack: () => void) => {
-    if (+timer.second == 0 || Number.isNaN(+timer.second-1)) {
-        if (+timer.minute > 0) {
-            if (+timer.second == 0) {
-                setTimeout(() => {
-                    dispatch(minusMinute())
-                    dispatch(setSecond({second: '60'}))
-                }, 1000);
-            } else {
-                dispatch(minusMinute())
-                dispatch(setSecond({second: '59'}))
-            }
-        } else if (onPlay) {
-            finishCallBack()
-        };
-    }
-}
-
-const handlingTimerStart = (onPlay: any, dispatch: any, soundRef: any) => {
-    const startTimer = () => {
-        BackgroundTimer.runBackgroundTimer(() => {
-            dispatch(minusSecond())
-        }, 1000)
-    }
-
-    if (onPlay) {
-        console.log('background sound on!');
-        startTimer();
-        soundRef.current.setNumberOfLoops(-1).play();
-    } else {
-        console.log('background sound off..');
-        BackgroundTimer.stopBackgroundTimer();
-        soundRef.current.stop(); 
-    }
-}
-
-const Time = ({isSetTimer, dispatch, setModalVisible, timer}: TimeType) => {
-    return (
-        <Clickable 
-            disabled={isSetTimer ? true : false}
-            onPress={() => {
-                dispatch(setTimer({minute: '00', second: '00'}))
-                setModalVisible(true)
-            }}>
-            <TimeTitle>{timer.minute+':'+timer.second}</TimeTitle>
-        </Clickable>
-    )
+const Time = ({ isSetTimer, dispatch, setModalVisible, timer }: TimeType) => {
+	return (
+		<Clickable
+			disabled={isSetTimer ? true : false}
+			onPress={() => {
+				dispatch(setTimer({ minute: "00", second: "00" }))
+				setModalVisible(true)
+			}}
+		>
+			<TimeTitle>{timer.minute + ":" + timer.second}</TimeTitle>
+		</Clickable>
+	)
 }
 
 interface TimeType {
-    isSetTimer: any
-    dispatch: any
-    setModalVisible: any
-    timer: any
+	isSetTimer: any
+	dispatch: any
+	setModalVisible: any
+	timer: any
 }
 
-const Buttons = ({isSetTimer, dispatch, setIsSetTimer, onPlay, setPlay}: ButtonsType) => {
-    return (
-        <ButtonContainer style={{opacity: isSetTimer ? 100 : 0}}>
-            <TextCircleButton label='취소' onPress={() => {
-                MyAlert(dispatch, setIsSetTimer, setPlay)
-                }}/>
-            <Spacer/>
-            <PlayButton onPlay={onPlay} playable={isSetTimer} onPress={() => {setPlay(!onPlay)}}/>
-        </ButtonContainer>
-    )
-}
 
 interface ButtonsType {
-    isSetTimer: any
-    dispatch: any
-    setIsSetTimer: any
-    onPlay: any,
-    setPlay: any
+	isSetTimer: any
+	onPlay: any
+    leftOnPress: () => void
+    rightOnPress: () => void
 }
+
+const Buttons = ({
+	isSetTimer,
+	onPlay,
+    leftOnPress,
+    rightOnPress,
+}: ButtonsType) => {
+	return (
+		<ButtonContainer style={{ opacity: isSetTimer ? 100 : 0 }}>
+			<TextCircleButton label="취소" onPress={leftOnPress} />
+			<Spacer />
+			<PlayButton
+				onPlay={onPlay}
+				playable={isSetTimer}
+				onPress={rightOnPress}
+			/>
+		</ButtonContainer>
+	)
+}
+
 
 interface AlarmTitleProps {
-    endTime: string
+	endTime: string
 }
 
-const AlarmTitle = ({endTime}: AlarmTitleProps) => {
-    return (
-        <AlramContainer>
-            <AlramIcon source={require('../assets/images/ic_alram.png')}/>
-            <SubTitle>{endTime}</SubTitle> 
-        </AlramContainer>
-    );
+const AlarmTitle = ({ endTime }: AlarmTitleProps) => {
+	return (
+		<AlramContainer>
+			<AlramIcon source={require("../assets/images/ic_alram.png")} />
+			<SubTitle>{endTime}</SubTitle>
+		</AlramContainer>
+	)
 }
 
 const AlramContainer = styled.View`
-    flex-direction: row;
-    align-items: center;
-    margin-top: 70px;
+	flex-direction: row;
+	align-items: center;
+	margin-top: 70px;
 `
 
 const AlramIcon = styled.Image`
-   width: 12px;
-   height: 14px;
-   margin-right: 8px;
-`
-
-const CatImage = styled.Image`
-    margin-top: 20px;
-    width: 240px;
-    height: 220px;
+	width: 12px;
+	height: 14px;
+	margin-right: 8px;
 `
 
 const SubTitle = styled.Text`
-    font-size: 20px;
-    color: ${colors.text_gray_100};
+	font-size: 20px;
+	color: ${colors.text_gray_100};
 `
 
 const TimeTitle = styled.Text`
-    font-size: 80px;
-    color: ${colors.text_gray_100};
+	font-size: 80px;
+	color: ${colors.text_gray_100};
 `
-
-
-
-
